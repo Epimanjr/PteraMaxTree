57b9189605d7e31ce2cb733fb5463a0043d5fbf1
Maxime <Maxime@plg-20218.connectify>
2015-02-09 12:26:35 +0100

    Folder management.

---
+++ src/main/Folder.java
@@ -0,0 +1,16 @@
+/*
+ * Class folder.
+ */
+package main;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Maxime BLAISE
+ */
+public class Folder {
+    
+    private ArrayList<File> listFiles = new ArrayList<>();
+}
